4f63a7f4e300b52932c4a73f54b46b62a9b1b2cb
Anthony Panagi <ant.panagi@gmail.com>
2020-12-01 16:11:10 +0000

    Add animation to wall nodes

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -28,6 +28,32 @@ const wallNode = css`
   background-color: ${WALL_COLOR};
   border-right: 1px solid ${WALL_COLOR};
   border-bottom: 1px solid ${WALL_COLOR};
+
+  animation-name: wallAnimation;
+  animation-duration: 0.3s;
+  animation-timing-function: ease-out;
+  animation-delay: 0;
+  animation-direction: alternate;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  animation-play-state: running;
+
+  @keyframes wallAnimation {
+    0% {
+      transform: scale(0.3);
+      background-color: rgb(12, 53, 71);
+    }
+
+    50% {
+      transform: scale(1.2);
+      background-color: rgb(12, 53, 71);
+    }
+
+    100% {
+      transform: scale(1);
+      background-color: ${WALL_COLOR};
+    }
+  }
 `;
 
 const startNode = css`
